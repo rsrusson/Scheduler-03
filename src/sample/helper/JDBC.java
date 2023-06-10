a9e600fa84bcbd716775ab3996553201b49dfcd3
Ryan Russon <rsrusson1@gmail.com>
2023-06-10 15:37:07 -0600

    VM JDBC connection = commit 2

--- src/sample/helper/JDBC.java
+++ src/sample/helper/JDBC.java
@@ -1,6 +1,10 @@
 package sample.helper;
 
-public class JDBC {
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public abstract class JDBC {
+
     private static final String protocol = "jdbc";
     private static final String vendor = ":mysql:";
     private static final String location = "//localhost/";
