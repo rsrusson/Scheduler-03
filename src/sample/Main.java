235f87635dded2bcd0600f70e2e2558de7590757
Ryan Russon <rsrusson1@gmail.com>
2023-06-10 15:48:09 -0600

    VM JDBC connection = commit 3

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,7 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import sample.helper.JDBC;
 
 public class Main extends Application {
 
@@ -18,6 +19,10 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        JDBC.openConnection();
         launch(args);
+
+
+        JDBC.closeConnection();
     }
 }
